396d544678b6be95cba4cc4b86a63589dfeb9b28
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-01-27 13:51:05 +0700

    ads

--- app/Models/tipe.php
+++ app/Models/tipe.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Tambahkan baris ini!
 use Illuminate\Database\Eloquent\Model;
 
 class Tipe extends Model
 {
- use HasFactory;
+    use HasFactory; // Sekarang baris ini akan berfungsi
 
     protected $fillable = [
         'project_id', 'nama_tipe', 'harga', 'luas_tanah', 
@@ -16,6 +17,5 @@ class Tipe extends Model
     public function project()
     {
         return $this->belongsTo(Project::class);
-        //
     }   
 }
